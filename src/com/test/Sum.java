8ef988caa723923d140c394a28d29d04eac79a37
谢文文 <xiewenwen@xiewenwendeMacBook-Pro.local>
2019-11-20 22:33:47 +0800

    my test

---
+++ src/com/test/Sum.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Sum {
+    private int num1=1;
+    private int num2=2;
+    public int sum(int num1,int num2){
+        int num3=num1+num2;
+        return num3;
+    }
+    public void testString(){
+        String words="love ashin";
+        System.out.println(words);
+    }
+
+}
---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+
+    public static void main(String[] args){
+        Sum sum=new Sum();
+
+        System.out.println(".........."+sum.sum(1,2));
+        sum.testString();
+    }
+
+}
